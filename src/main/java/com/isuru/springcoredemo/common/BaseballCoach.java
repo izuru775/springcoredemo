c38d38e9ac7fe5427a6c071e28ccb7b126d31434
Isuru Adikaram <izuru775@gmail.com>
2025-03-15 10:16:44 +1100

    Lazy initialization completed

--- src/main/java/com/isuru/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/isuru/springcoredemo/common/BaseballCoach.java
@@ -1,10 +1,15 @@
 package com.isuru.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class BaseballCoach implements Coach {
 
+    public BaseballCoach(){
+        System.out.println("In constructor: "+ getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes in batting practice";
--- src/main/java/com/isuru/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/isuru/springcoredemo/common/TrackCoach.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TrackCoach implements Coach{
+    public TrackCoach(){
+        System.out.println("In constructor: "+ getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k!";
