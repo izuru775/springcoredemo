ba0284c3b427461e32a3b29f01d09565ca6b162a
Isuru Adikaram <izuru775@gmail.com>
2025-03-15 09:16:26 +1100

    Primary added to designate which bean to use

--- src/main/java/com/isuru/springcoredemo/rest/DemoController.java
+++ src/main/java/com/isuru/springcoredemo/rest/DemoController.java
@@ -2,7 +2,6 @@ package com.isuru.springcoredemo.rest;
 
 import com.isuru.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public void DemoController( Coach theCoach){
         myCoach = theCoach;
     }
 
